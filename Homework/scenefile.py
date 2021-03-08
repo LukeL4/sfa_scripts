d4455d474128a4f8c7213833f2db4e8bc76f2414
LukeL4 <lbl190000@utdallas.edu>
2021-03-07 22:39:05 -0600

    Finished video 7 modeuel 5 added saving feature added in excpetion to allow creation of folders

--- Homework/scenefile.py
+++ Homework/scenefile.py
@@ -1,4 +1,9 @@
-from pymel.core.system  import Path
+import logging
+
+import pymel.core as pmc
+from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
 
 
 class SceneFile(object):
@@ -10,6 +15,7 @@ class SceneFile(object):
         self.ver = 1
         self.ext = '.ma'
         self._init_from_path(path)
+
     @property
     def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
@@ -26,11 +32,22 @@ class SceneFile(object):
         path = Path(path)
         self.folder_path = path.parent
         self.ext = path.ext
-        self.descriptor, self.task, ver, = path.stripext().split("_")
+        self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
 
+    def save(self):
+         """Saves the scene file.
 
-scene_file = SceneFile("D:/tank_model_v001.ma")
-print(scene_file.path)
-print(scene_file.filename)
+         Returns:
+             Path: the path to the scene file if successful
+         """
+         try:
+            return pmc.system.saveAs(self.path)
+         except RuntimeError as err:
+            log.warning("Missing directories in path. Creating directories...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
+
+
+
 
