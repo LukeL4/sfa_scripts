d9f1f6a274556453d8e4b3d4194bd3096da74a52
LukeL4 <lbl190000@utdallas.edu>
2021-04-21 17:23:03 -0500

    added in functinality for instancing to only a percentage of selection. created a double spin box to control this percentage

--- src/scatter.py
+++ src/scatter.py
@@ -54,6 +54,8 @@ class ScatterUI(QtWidgets.QDialog):
         self.scaleY_lbl.setStyleSheet("font:  bold 10px")
         self.scaleZ_lbl = QtWidgets.QLabel("Scale Z")
         self.scaleZ_lbl.setStyleSheet("font:  bold 10px")
+        self.density_lbl = QtWidgets.QLabel("Scatter density")
+        self.density_lbl.setStyleSheet("font:  bold 10px")
 
         """spin box creation"""
         self.spin_xRotMin = QtWidgets.QSpinBox()
@@ -68,10 +70,12 @@ class ScatterUI(QtWidgets.QDialog):
         self.spin_yScaleMax = QtWidgets.QSpinBox()
         self.spin_zScaleMin = QtWidgets.QSpinBox()
         self.spin_zScaleMax = QtWidgets.QSpinBox()
+        self.spin_density = QtWidgets.QDoubleSpinBox()
+
 
     def scatter_button(self):
         self.scatter_btn = QtWidgets.QPushButton("Scatter")
-        self.gridlay.addWidget(self.scatter_btn, 9, 0)
+        self.gridlay.addWidget(self.scatter_btn, 10, 0)
         self.scatter_btn.setMaximumWidth(80)
         self.scatter_btn.clicked.connect(self.scatter_slot)
 
@@ -100,7 +104,9 @@ class ScatterUI(QtWidgets.QDialog):
         self.spin_zScaleMin.setMaximumWidth(100)
         self.spin_zScaleMax.setRange(1, 10)
         self.spin_zScaleMax.setMaximumWidth(100)
-
+        self.spin_density.setRange(0, 1)
+        self.spin_density.setMaximumWidth(100)
+        self.spin_density.setSingleStep(.01)
 
     def interactables_layout(self):
         self.gridlay.addWidget(self.spin_xRotMin, 3, 1)
@@ -115,6 +121,8 @@ class ScatterUI(QtWidgets.QDialog):
         self.gridlay.addWidget(self.spin_yScaleMax, 7, 2)
         self.gridlay.addWidget(self.spin_zScaleMin, 8, 1)
         self.gridlay.addWidget(self.spin_zScaleMax, 8, 2)
+        self.gridlay.addWidget(self.spin_density, 9, 1)
+
 
 
     def label_layout(self):
@@ -130,6 +138,7 @@ class ScatterUI(QtWidgets.QDialog):
         self.gridlay.addWidget(self.scaleX_lbl, 6, 0)
         self.gridlay.addWidget(self.scaleY_lbl, 7, 0)
         self.gridlay.addWidget(self.scaleZ_lbl, 8, 0)
+        self.gridlay.addWidget(self.density_lbl, 9, 0)
         self.setLayout(self.gridlay)
 
     @QtCore.Slot()
@@ -150,6 +159,8 @@ class ScatterUI(QtWidgets.QDialog):
         self.scatter.yScaleMax = self.spin_yScaleMax.value()
         self.scatter.zScaleMin = self.spin_zScaleMin.value()
         self.scatter.zScaleMax = self.spin_zScaleMax.value()
+        self.scatter.spin_density = self.spin_density.value()
+
 
 
 # change from having 2 types of naming conventions to only one using refactor
@@ -169,6 +180,7 @@ class Scatter(object):
         self.yScaleMax = 1
         self.zScaleMin = 1
         self.zScaleMax = 1
+        self.spin_density = 1.0
 
 
     def scatter_objects(self):
@@ -179,10 +191,17 @@ class Scatter(object):
 
         vtx_selection = cmds.polyListComponentConversion(instanceTo, toVertex=True)
         vtx_selection = cmds.filterExpand(vtx_selection, selectionMask=31)
+# only use a percent of selcted vets
+        len(vtx_selection)
+        random_amount = int(round(len(vtx_selection) * self.spin_density))
+        print(random_amount)
+        percentage_selection = random.sample(vtx_selection, k=random_amount)
+        cmds.select(percentage_selection)
 
-        cmds.select(vtx_selection)
+        #cmds.select(vtx_selection)
 
-        for vtx in vtx_selection:
+        for vtx in percentage_selection:
+        #for vtx in vtx_selection:
             scatter_instance = cmds.instance(toInstance, name="pInstance *")
             pos = cmds.xform([vtx], query=True, translation=True)
             cmds.xform(scatter_instance, translation=pos)
