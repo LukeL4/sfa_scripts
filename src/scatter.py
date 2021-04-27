024bf3a1914c2427ac5d0bc7c1dffd0251d3296c
LukeL4 <lbl190000@utdallas.edu>
2021-04-27 15:29:11 -0500

    added in ui for putting an position offset on instances. Added in the move code for itt but not working

--- src/scatter.py
+++ src/scatter.py
@@ -3,6 +3,7 @@ from PySide2 import QtWidgets, QtCore
 from shiboken2 import wrapInstance
 import maya.cmds as cmds
 import random
+import pymel.core as pm
 
 def maya_main_window():
     """return the maya main window widget"""
@@ -56,6 +57,18 @@ class ScatterUI(QtWidgets.QDialog):
         self.scaleZ_lbl.setStyleSheet("font:  bold 10px")
         self.density_lbl = QtWidgets.QLabel("Scatter density")
         self.density_lbl.setStyleSheet("font:  bold 10px")
+        self.density_guide_lbl = QtWidgets.QLabel("1 = 100%")
+        self.density_guide_lbl.setStyleSheet("font:  bold 10px")
+        self.offset_lbl = QtWidgets.QLabel("Create Offset")
+        self.offset_lbl.setStyleSheet("font:  bold 12px")
+        self.offset_x_lbl = QtWidgets.QLabel("Offset X")
+        self.offset_x_lbl.setStyleSheet("font:  bold 10px")
+        self.offset_y_lbl = QtWidgets.QLabel("Offset Y")
+        self.offset_y_lbl.setStyleSheet("font:  bold 10px")
+        self.offset_z_lbl = QtWidgets.QLabel("Offset Z")
+        self.offset_z_lbl.setStyleSheet("font:  bold 10px")
+
+
 
         """spin box creation"""
         self.spin_xRotMin = QtWidgets.QSpinBox()
@@ -71,11 +84,19 @@ class ScatterUI(QtWidgets.QDialog):
         self.spin_zScaleMin = QtWidgets.QSpinBox()
         self.spin_zScaleMax = QtWidgets.QSpinBox()
         self.spin_density = QtWidgets.QDoubleSpinBox()
+        self.spin_x_offset_min = QtWidgets.QSpinBox()
+        self.spin_x_offset_max = QtWidgets.QSpinBox()
+        self.spin_y_offset_min = QtWidgets.QSpinBox()
+        self.spin_y_offset_max = QtWidgets.QSpinBox()
+        self.spin_z_offset_min = QtWidgets.QSpinBox()
+        self.spin_z_offset_max = QtWidgets.QSpinBox()
+
+
 
 
     def scatter_button(self):
         self.scatter_btn = QtWidgets.QPushButton("Scatter")
-        self.gridlay.addWidget(self.scatter_btn, 10, 0)
+        self.gridlay.addWidget(self.scatter_btn, 14, 0)
         self.scatter_btn.setMaximumWidth(80)
         self.scatter_btn.clicked.connect(self.scatter_slot)
 
@@ -107,6 +128,21 @@ class ScatterUI(QtWidgets.QDialog):
         self.spin_density.setRange(0, 1)
         self.spin_density.setMaximumWidth(100)
         self.spin_density.setSingleStep(.01)
+        self.spin_density.setValue(1)
+        self.spin_x_offset_min.setRange(0, 10)
+        self.spin_x_offset_min.setMaximumWidth(100)
+        self.spin_x_offset_max.setRange(0, 10)
+        self.spin_x_offset_max.setMaximumWidth(100)
+        self.spin_y_offset_min.setRange(0, 10)
+        self.spin_y_offset_min.setMaximumWidth(100)
+        self.spin_y_offset_max.setRange(0, 10)
+        self.spin_y_offset_max.setMaximumWidth(100)
+        self.spin_z_offset_min.setRange(0, 10)
+        self.spin_z_offset_min.setMaximumWidth(100)
+        self.spin_z_offset_max.setRange(0, 10)
+        self.spin_z_offset_max.setMaximumWidth(100)
+
+
 
     def interactables_layout(self):
         self.gridlay.addWidget(self.spin_xRotMin, 3, 1)
@@ -122,6 +158,13 @@ class ScatterUI(QtWidgets.QDialog):
         self.gridlay.addWidget(self.spin_zScaleMin, 8, 1)
         self.gridlay.addWidget(self.spin_zScaleMax, 8, 2)
         self.gridlay.addWidget(self.spin_density, 9, 1)
+        self.gridlay.addWidget(self.spin_x_offset_min, 11, 1)
+        self.gridlay.addWidget(self.spin_x_offset_max, 11, 2)
+        self.gridlay.addWidget(self.spin_y_offset_min, 12, 1)
+        self.gridlay.addWidget(self.spin_y_offset_max, 12, 2)
+        self.gridlay.addWidget(self.spin_z_offset_min, 13, 1)
+        self.gridlay.addWidget(self.spin_z_offset_max, 13, 2)
+
 
 
 
@@ -139,6 +182,14 @@ class ScatterUI(QtWidgets.QDialog):
         self.gridlay.addWidget(self.scaleY_lbl, 7, 0)
         self.gridlay.addWidget(self.scaleZ_lbl, 8, 0)
         self.gridlay.addWidget(self.density_lbl, 9, 0)
+        self.gridlay.addWidget(self.density_guide_lbl, 9, 2)
+        self.gridlay.addWidget(self.offset_lbl, 10, 0)
+        self.gridlay.addWidget(self.offset_x_lbl, 11, 0)
+        self.gridlay.addWidget(self.offset_y_lbl, 12, 0)
+        self.gridlay.addWidget(self.offset_z_lbl, 13, 0)
+
+
+
         self.setLayout(self.gridlay)
 
     @QtCore.Slot()
@@ -160,6 +211,14 @@ class ScatterUI(QtWidgets.QDialog):
         self.scatter.zScaleMin = self.spin_zScaleMin.value()
         self.scatter.zScaleMax = self.spin_zScaleMax.value()
         self.scatter.spin_density = self.spin_density.value()
+        self.scatter.spin_x_offset_min = self.spin_x_offset_min.value()
+        self.scatter.spin_x_offset_max = self.spin_x_offset_max.value()
+        self.scatter.spin_y_offset_min = self.spin_y_offset_min.value()
+        self.scatter.spin_y_offset_max = self.spin_y_offset_max.value()
+        self.scatter.spin_z_offset_min = self.spin_z_offset_min.value()
+        self.scatter.spin_z_offset_max = self.spin_z_offset_max.value()
+
+
 
 
 
@@ -181,6 +240,13 @@ class Scatter(object):
         self.zScaleMin = 1
         self.zScaleMax = 1
         self.spin_density = 1.0
+        self.spin_x_offset_min = 0
+        self.spin_x_offset_max = 0
+        self.spin_y_offset_min = 0
+        self.spin_y_offset_max = 0
+        self.spin_z_offset_min = 0
+        self.spin_z_offset_max = 0
+
 
 
     def scatter_objects(self):
@@ -188,6 +254,9 @@ class Scatter(object):
 
         toInstance = order[0]
         instanceTo = order[1:]
+        #mesh_vert = pm.MeshVertex(vert)
+       # mesh_vert.getNormal()
+
 
         vtx_selection = cmds.polyListComponentConversion(instanceTo, toVertex=True)
         vtx_selection = cmds.filterExpand(vtx_selection, selectionMask=31)
@@ -198,13 +267,13 @@ class Scatter(object):
         percentage_selection = random.sample(vtx_selection, k=random_amount)
         cmds.select(percentage_selection)
 
-        #cmds.select(vtx_selection)
 
         for vtx in percentage_selection:
-        #for vtx in vtx_selection:
+
             scatter_instance = cmds.instance(toInstance, name="pInstance *")
             pos = cmds.xform([vtx], query=True, translation=True)
             cmds.xform(scatter_instance, translation=pos)
+            #constrain = cmds.normalConstraint([toInstance, instanceTo])
 
             xRot = random.uniform(self.xRotMin, self.xRotMax)
             yRot = random.uniform(self.yRotMin, self.yRotMax)
@@ -217,3 +286,11 @@ class Scatter(object):
             zScale = random.uniform(self.zScaleMin, self.zScaleMax)
 
             cmds.scale(xScale, yScale, zScale, scatter_instance)
+
+            """
+            x_tran = random.uniform(self.spin_x_offset_min, self.spin_x_offset_max)
+            y_tran = random.uniform(self.spin_y_offset_min, self.spin_y_offset_max)
+            z_tran = random.uniform(self.spin_z_offset_min, self.spin_z_offset_max)
+
+            cmds.moveXYZ(x_tran, y_tran, z_tran, scatter_instance)
+"""
